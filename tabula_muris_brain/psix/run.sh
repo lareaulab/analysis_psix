c6a3c6f4cfb1ec69e41efcede41e27ba7246f2d7
cfbuenabadn <cfbuenabadn@berkeley.edu>
2021-01-31 18:28:23 -0800

    getting tabula muris data

--- tabula_muris_brain/psix/run.sh
+++
@@ -1,8 +0,0 @@
-DATA='/mnt/lareaulab/cfbuenabadn/data_sc_regulation/data_autocorrelation'
-
-#python ~/psix/utils/psix.py -psi $DATA/tiklova_neurogenesis/skipped_exons_psi.tab -mrna $DATA/tiklova_neurogenesis/mrna_per_event.tab -rd $DATA/tiklova_neurogenesis/rd_pc2.tab -o tiklova_neurogenesis -k 100
-
-python ~/psix/utils/psix.py -psi $DATA/brain_neurons/skipped_exons_psi.tab -mrna $DATA/brain_neurons/mrna_per_event.tab -rd ~/analysis_psix/scvi_runs/tabula_muris_scvi10_rd.tab -o scvi10 -k 100
-
-#python ~/psix/utils/psix.py -psi $DATA/tiklova_neurogenesis/skipped_exons_psi.tab -mrna $DATA/tiklova_neurogenesis/mrna_per_event.tab -rd ~/analysis_psix/scvi_runs/tiklova_scvi5_batch_rd.tab -o scvi5_batch -k 100
-
