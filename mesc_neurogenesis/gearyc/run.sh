7645a6c6c683ef04d83f05a9a047764c117e21d5
cfbuenabadn <cfbuenabadn@berkeley.edu>
2021-07-10 11:20:43 -0700

    cleaning iPSC notebook

--- mesc_neurogenesis/gearyc/run.sh
+++ mesc_neurogenesis/gearyc/run.sh
@@ -1,1 +1,1 @@
-python ~/psix_project/psix/utils/geary_c.py -psi ../data/preprocess/tables/psi.tab.gz -rd ../data/preprocess/tables/pc2_rd.tab.gz -p 2000 -k 30 -o gearyc
+python ~/psix_project/analysis_psix/utils/geary_c.py -psi ../data/preprocess/tables/psi.tab.gz -rd ../data/preprocess/tables/pc2_rd.tab.gz -p 2000 -k 30 -o gearyc
--- midbrain_development/gearyc/run.sh
+++ midbrain_development/gearyc/run.sh
@@ -1,1 +1,1 @@
-python ~/psix_project/psix/utils/geary_c.py -psi ../data/preprocess/tables/psi_3D.tab.gz -rd ../data/preprocess/tables/pc3_rd.tab.gz -p 2000 -k 100 -o gearyc
+python ~/psix_project/analysis_psix/utils/geary_c.py -psi ../data/psi_3D.tab.gz -rd ../data/preprocess/tables/pc3_rd.tab.gz -p 2000 -k 100 -o gearyc
